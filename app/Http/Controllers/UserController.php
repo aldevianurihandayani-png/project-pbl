e9b39a56a7aca90e9c2bd5394cc475d15daa9ba9
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 16:12:31 +0800

    initialize UserController with basic structure

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register (Request $request) {
+        $incomingFields = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        return 'Hello from my controller';
+    }
+}
