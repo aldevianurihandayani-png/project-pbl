995b41c7168ff77e5cb03bc53c0e002b396d0edf
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 16:00:21 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/home"> Home </a>
+    <a href="/about"> About </a>
+    <h1> Halaman Home </h1>
+
+    <div style="border: 3px solid black;">
+        <h2>Register:</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="text" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+</body>
+</html>
