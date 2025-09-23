a30e616fb335ab78966189f5ee94bdf8f26308d9
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 19:37:22 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
 
+// Halaman Home
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
+// Halaman About
 Route::get('/about', function () {
     return view('about');
 });
 
-Route::post('/register', [UserController::class,'register']);
+// Proses Register
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+// Halaman Login
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+// Logout
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Halaman Beranda (setelah login)
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
