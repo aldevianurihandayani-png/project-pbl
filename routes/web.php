20fc5a58c7fe037e4cc42955347dacf5de977bb7
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 16:25:01 +0800

    fix: update home view to welcome and add about route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
 
 Route::post('/register', [UserController::class,'register']);
