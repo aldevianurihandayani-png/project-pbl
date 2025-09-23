b832885fdbe11c753caa36d70629090623d6c60d
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 16:20:41 +0800

    add UserController and PostController imports in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::post('/register', [UserController::class,'register']);
