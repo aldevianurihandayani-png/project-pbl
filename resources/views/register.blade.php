5adacbffa46e3ac85d14175d30bec1255dd20ca6
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-23 20:01:35 +0800

    refactor: improve register view structure and error handling

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,28 +6,33 @@
     <title>Register</title>
 </head>
 <body>
-    <h2>Form Register</h2>
+    <h1>Form Register</h1>
 
-    {{-- Form kirim ke route POST /register --}}
-    <form action="{{ url('/register') }}" method="POST">
+    {{-- Tampilkan pesan sukses jika ada --}}
+    @if(session('success'))
+        <p style="color: green;">{{ session('success') }}</p>
+    @endif
+
+    {{-- Tampilkan error validasi --}}
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="/register" method="POST">
         @csrf
-
-        <div>
-            <label for="name">Name:</label><br>
-            <input type="text" id="name" name="name" placeholder="Name" required>
-        </div>
-
-        <div>
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email" placeholder="Email" required>
-        </div>
-
-        <div>
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password" placeholder="Password" required>
-        </div>
-
+        <input type="text" name="name" placeholder="Name" value="{{ old('name') }}"><br><br>
+        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
         <button type="submit">Register</button>
     </form>
+
+    <br>
+    <a href="/login">Sudah punya akun? Login di sini</a>
 </body>
 </html>
