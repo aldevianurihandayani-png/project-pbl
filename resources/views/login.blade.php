dadf9a1d00fb7df7b66f5e4e3aa3d9dc0b36ee01
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-09-24 11:37:38 +0800

    login.blade

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistem PBL</title>
+</head>
+<body>
+    <h2>Login</h2>
+
+    @if ($errors->any())
+        <div style="color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('login.authenticate') }}" method="POST">
+        @csrf
+        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
