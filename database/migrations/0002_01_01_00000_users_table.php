62d4eacd529a5794443fa090b4e2af20357c9ca7
Noorma <noorma@mhs.politala.ac.id>
2025-10-01 09:52:54 +0800

    admin dashboard

---
+++ database/migrations/0002_01_01_00000_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Jalankan migrasi.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // pastikan kolom 'password' sudah ada di tabel users
+            if (!Schema::hasColumn('users', 'role')) {
+                $table->string('role')->default('user')->after('password'); 
+            }
+        });
+    }
+
+    /**
+     * Rollback migrasi.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'role')) {
+                $table->dropColumn('role');
+            }
+        });
+    }
+};
