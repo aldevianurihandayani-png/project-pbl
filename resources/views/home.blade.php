913e05d929e3ec9509973fc9db56cafe9bfaa7af
wikeewidia <wikeewidia@gmail.com>
2025-10-01 11:04:39 +0800

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,36 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>SIMAP Politala</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    body {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-    }
-    .navbar-custom {
-      background-color: #002868; /* biru tua */
-    }
-    .navbar-custom .nav-link,
-    .navbar-custom .navbar-brand {
-      color: #fff;
-    }
-    .navbar-custom .nav-link:hover {
-      color: #d1d5db; /* abu hover */
-    }
-    .brand-subtitle {
-      font-size: .8rem;
-      display: block;
-      line-height: 1;
-    }
-    footer {
-      margin-top: auto; /* selalu di bawah */
-      background: #002868;
-      color: #fff;
-      text-align: center;
-      padding: 10px 0;
-      font-size: .9rem;
-    }
-  </style>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
