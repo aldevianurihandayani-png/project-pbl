c220263e8d5499929e97032b1fbf2f999602222a
wikeewidia <wikeewidia@gmail.com>
2025-10-01 09:25:37 +0800

    mff header lg

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,20 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Manajemen PBL</title>
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-
 </head>
 <body>
-   <header>
-    <div class="navbar">
-        <div class="logo">SISTEM INFORMASI MANAJEMEN PBL</div>
+
+<header>
+    <!-- Bar Judul -->
+    <div class="header-top">
+        <div class="brand">SISTEM INFORMASI MANAJEMEN PBL</div>
+    </div>
+
+    <!-- Bar Menu -->
+    <div class="header-menu">
         <nav>
-            <a href="{{ url('home') }}">Home</a>
-            <a href="{{ url('about') }}">Tentang</a>
-            <a href="{{ url('logbook') }}">Catatan</a>
-            <a href="{{ url('group') }}">Kelompok</a>
-            <a href="{{ url('login') }}">Login</a>
+            <a href="{{ url('/') }}">Home</a>
+            <a href="{{ url('/about') }}">Tentang</a>
+            <a href="{{ url('/logbook') }}">Catatan</a>
+            <a href="{{ url('/group') }}">Kelompok</a>
+            <a href="{{ url('/login') }}">Login</a>
         </nav>
     </div>
 </header>
+
 </body>
 </html>
