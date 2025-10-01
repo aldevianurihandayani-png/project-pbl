c8de8336a180693e0d4fff5861f604fb3a6103ee
wikeewidia <wikeewidia@gmail.com>
2025-10-01 11:15:16 +0800

    ya css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
   <title>SIMAP Politala</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
 <body>
 
@@ -30,11 +31,6 @@
     </div>
   </div>
 </div>
-
-<!-- Footer -->
-<footer>
-  © 2025 Sistem PBL - Jurusan Teknologi Informasi. All rights reserved.
-</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
