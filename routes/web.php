39bd0d8165703d36454f79530b6b67b8ed64c534
wikeewidia <wikeewidia@gmail.com>
2025-10-01 09:47:09 +0800

    logbook post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LogbookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
@@ -22,6 +23,8 @@ Route::get('/about', function () {
 Route::get('/logbook', function () {
     return view('logbook');
 })->name('logbook');
+Route::post('/logbook', [LogbookController::class, 'logbook'])->name('logbook.post');
+
 
 // ==============================
 // Autentikasi
