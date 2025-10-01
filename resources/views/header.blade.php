7081052440da96c496dbd1f80d84db2c6689527f
wikeewidia <wikeewidia@gmail.com>
2025-10-01 10:43:36 +0800

    ya itu

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Manajemen PBL</title>
-    <link rel="stylesheet" href="{{ asset('header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
 <body>
 
