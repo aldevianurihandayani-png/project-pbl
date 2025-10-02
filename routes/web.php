3e613a92443582b710d798192c3250c871a68885
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-10-02 20:08:32 +0800

    buat nambah contact controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\LogbookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ContactController; 
 
 // ==============================
 // Halaman Publik
@@ -18,6 +20,10 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+
+// Halaman Contact
+Route::get('/contact', fn() => view('contact'))->name('contact');
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
 // Halaman daftar logbook
 Route::get('/logbook', [LogbookController::class, 'index'])->name('logbook.index');
@@ -57,8 +63,6 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-use App\Http\Controllers\MahasiswaController;
 
 Route::resource('mahasiswa', MahasiswaController::class);
 // sekarang /mahasiswa, /mahasiswa/create, /mahasiswa/{id}/edit, dst.
