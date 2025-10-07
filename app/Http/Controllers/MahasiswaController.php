5709402499bb794268f6910bdef6f88f03c457f4
Noorma <noorma@mhs.politala.ac.id>
2025-10-07 10:07:59 +0800

    kd bisa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -1,28 +1,33 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use App\Models\Mahasiswa;
-use Illuminate\Http\Request;
-
 class MahasiswaController extends Controller
 {
     public function index(Request $request)
     {
         $search = $request->search;
 
-        $mahasiswa = Mahasiswa::when($search, function ($q) use ($search) {
+        $mahasiswas = ahasiswa::when($search, function ($q) use ($search) {
                 $q->where('nim', 'like', "%{$search}%")
                   ->orWhere('nama', 'like', "%{$search}%")
                   ->orWhere('angkatan', 'like', "%{$search}%")
                   ->orWhere('no_hp', 'like', "%{$search}%");
             })
             ->latest()
-            ->paginate(10);
+            ->paginate(10)
+            ->withQueryString();
 
-        // kirim variabel bernama $mahasiswa (bukan $mahasiswas)
-        return view('mahasiswa.index', compact('mahasiswa', 'search'));
+        // pakai nama variabel konsisten dgn view: $mahasiswas
+        return view('mahasiswa.index', [
+            'mahasiswas' => $mahasiswas,
+            'search' => $search,
+        ]);
     }
 
-    // method resource lain (create/store/edit/update/destroy) menyusul...
+    public function dashboard()
+    {
+        // render view dashboard mahasiswa
+        return view('mahasiswa.dashboard');
+    }
+
+    // create/store/edit/update/destroy ... (lanjutkan punyamu)
 }
