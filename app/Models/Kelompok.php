4a6fcd6b6d66a7e151b3219015847fcd1b218bd5
Nama Kamu <email@example.com>
2025-10-16 10:05:43 +0800

    kelompok

--- app/Models/Kelompok.php
+++ app/Models/Kelompok.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kelompok extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'nama', 'kelas', 'anggota', 'dosen_pembimbing'];
-    protected $table = 'kelompok';   // <— jika tabel kamu singular
+        'nama', 'kelas', 'judul', 'ketua_kelompok', 'dosen_pembimbing', 'judul_proyek', 'nama_klien', 'anggota'
+    ];
+    protected $table = 'kelompoks';   // <— jika tabel kamu singular
 
 }
