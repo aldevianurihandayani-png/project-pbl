c5dd8d584ceabf7e28278ea93b08cb354db7f1e8
Nama Kamu <email@example.com>
2025-10-16 12:27:09 +0800

    dashboard

--- resources/views/dosenpenguji/dashboard.blade.php
+++ resources/views/dosenpenguji/dashboard.blade.php
@@ -105,7 +105,7 @@
       <div class="nav-title">Menu</div>
       <a href="{{ url('/dosenpenguji/dashboard') }}" class="active"><i class="fa-solid fa-house"></i>Dashboard</a>
       <a href="{{ url('/dosenpenguji/mahasiswa') }}"><i class="fa-solid fa-user-graduate"></i>Mahasiswa</a>
-      <a href="{{ url('/dosenpenguji/kelompom')}}"><i class="fa-solid fa-user-graduate"></i>Kelompok</a>
+      <a href="{{ url('/dosenpenguji/kelompok')}}"><i class="fa-solid fa-user-graduate"></i>Kelompok</a>
       <a href="{{ url('/dosenpenguji/penilaian') }}"><i class="fa-solid fa-users"></i>Penilaian</a>
       <a href="{{ url('/dosenpenguji/rubrik') }}"><i class="fa-solid fa-flag-checkered"></i>Rubrik</a>
       <a href="{{ url('/dosenpenguji/cpmk')}}"><i class="fa-solid fa-flag-checkered"></i>CPMK</a>
