87c1a941707f92c8b8ba097b78d9047ec6ce79cf
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-10-17 13:32:54 +0800

    user php utk nambah profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace App\Models;
 
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    // tabel users tidak punya created_at & updated_at
+    // Jika tabel users tidak memiliki created_at & updated_at
     public $timestamps = false;
 
     /**
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'email',
         'role',
         'password',
+        'nidn',
+        'prodi',
+        'profile_photo_path',
     ];
 
     /**
@@ -39,5 +42,22 @@ class User extends Authenticatable
         return [
             'password' => 'hashed', // otomatis bcrypt
         ];
+    }
+
+    /**
+     * Atribut tambahan saat model di-serialize.
+     */
+    protected $appends = ['avatar_url'];
+
+    /**
+     * Accessor: URL foto profil atau fallback.
+     */
+    public function getAvatarUrlAttribute()
+    {
+        if ($this->profile_photo_path) {
+            return asset('storage/' . $this->profile_photo_path);
+        }
+        // fallback: null atau bisa diganti path default avatar
+        return null;
     }
 }
