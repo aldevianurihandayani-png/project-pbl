b78463ebecd04e0d05a2206c41a00f44d26b9b41
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-10-17 13:31:35 +0800

    role middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -3,18 +3,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
+    public function handle($request, Closure $next, ...$roles) {
+        $user = Auth::user();
+        if (!$user) return redirect()->route('login');
+        if (!in_array($user->role, $roles, true)) {
+            return redirect()->route('home')->with('error','Akses ditolak.');
+        }
         return $next($request);
     }
 }
