becef269a125b6c57f6c443771a82b69f858e60b
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-10-17 13:32:06 +0800

    models kelompok

--- app/Models/Kelompok.php
+++ app/Models/Kelompok.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kelompok extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'nama', 'kelas', 'anggota', 'dosen_pembimbing'];
-    protected $table = 'kelompok';   // <— jika tabel kamu singular
+    protected $table = 'kelompoks';   // <— jika tabel kamu singular
 
 }
