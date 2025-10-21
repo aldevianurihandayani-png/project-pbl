94539229d74bef869af2060f32002ee6bd795316
Nama Kamu <email@example.com>
2025-10-21 23:17:52 +0800

    index

--- resources/views/admins/matakuliah/index.blade.php
+++ resources/views/admins/matakuliah/index.blade.php
@@ -23,11 +23,37 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td colspan="6" class="text-center">Hello World</td>
-                    </tr>
+                    @forelse ($matakuliah as $mk)
+                        <tr>
+                            <td>{{ $mk->kode_mk }}</td>
+                            <td>{{ $mk->nama_mk }}</td>
+                            <td>{{ $mk->sks }}</td>
+                            <td>{{ $mk->semester }}</td>
+                            <td>{{ $mk->dosen->name ?? 'N/A' }}</td>
+                            <td class="d-flex">
+                                <a href="{{ route('admins.matakuliah.edit', $mk) }}" class="btn btn-warning btn-sm mr-2">
+                                    <i class="fa fa-pen"></i> Edit
+                                </a>
+                                <form action="{{ route('admins.matakuliah.destroy', $mk) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus mata kuliah ini?')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        <i class="fa fa-trash"></i> Hapus
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6" class="text-center">Tidak ada data mata kuliah.</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
+        </div>
+         <div class="d-flex justify-content-center">
+            {{ dd(get_class($matakuliah)) }}
+            {{ $matakuliah->links() }}
         </div>
     </div>
 </div>
