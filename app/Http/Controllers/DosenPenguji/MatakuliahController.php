e0baff91caf6b80cd81a9682adcebefb30a868db
Aldevianuri Handayani <aldevianuri.handayani@mhs.politala.ac.id>
2025-10-22 01:05:54 +0800

    matakuliah dosen penguji

---
+++ app/Http/Controllers/DosenPenguji/MatakuliahController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\DosenPenguji;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MatakuliahController extends Controller
+{
+    public function index()
+    {
+        // This can be empty for now, or return a simple view
+        // The goal is just to make the class exist for route:list
+        return "MatakuliahController index";
+    }
+}
