e97307ee355f6d3a07f1b24f4772d35c08c2a559
Nama Kamu <email@example.com>
2025-10-21 15:18:30 +0800

    LOGin controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -38,31 +38,6 @@ class LoginController extends Controller
         // Sukses, regenerasi session
         $request->session()->regenerate();
 
-
-
-        if ($user->role === 'mahasiswa') {
-            return redirect()->route('mahasiswa.dashboard')->with('success', 'Login berhasil');
-        }
-
-        return redirect()->route('home')->with('success','Login berhasil');
-
-        // Logika redirect berdasarkan role
-        $role = $user->role;
-        
-        $routeMap = [
-            'admins' => 'admins.dashboard',
-            'dosen_pembimbing' => 'dosen.dashboard',
-            'dosen_penguji' => 'dosenpenguji.dashboard',
-            'jaminan_mutu' => 'jaminanmutu.dashboard',
-            'koordinator' => 'koordinator.dashboard',
-            'mahasiswa' => 'mahasiswa.dashboard',
-        ];
-
-        $redirectRoute = $routeMap[$role] ?? 'home';
-
-        return redirect()->route($redirectRoute)->with('success', 'Login berhasil');
-
-
         // Logika redirect berdasarkan role
         $role = $user->role;
         $redirectRoute = 'home'; // Default redirect
@@ -89,7 +64,6 @@ class LoginController extends Controller
         }
 
         return redirect()->route($redirectRoute)->with('success','Login berhasil');
-
     }
 
     public function logout(Request $request)
