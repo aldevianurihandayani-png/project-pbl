5362c3b52896e6c427f0f73935c4c6e2202650fc
Nama Kamu <email@example.com>
2025-10-21 22:11:45 +0800

    index

--- resources/views/admins/matakuliah/index.blade.php
+++ resources/views/admins/matakuliah/index.blade.php
@@ -1,7 +1,7 @@
 @include('admins.partials.header', ['title' => 'Manajemen Mata Kuliah'])
 
 <div class="card shadow mb-4">
-    <div class="card-header">
+        <div class="card-header">
         <a href="{{ route('admins.matakuliah.create') }}" class="btn btn-primary">Tambah Mata Kuliah</a>
     </div>
     <div class="card-body">
@@ -23,36 +23,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse ($matakuliah as $mk)
-                        <tr>
-                            <td>{{ $mk->kode_mk }}</td>
-                            <td>{{ $mk->nama_mk }}</td>
-                            <td>{{ $mk->sks }}</td>
-                            <td>{{ $mk->semester }}</td>
-                            <td>{{ $mk->dosen->name ?? 'N/A' }}</td>
-                            <td class="d-flex">
-                                <a href="{{ route('admins.matakuliah.edit', $mk) }}" class="btn btn-warning btn-sm mr-2">
-                                    <i class="fa fa-pen"></i> Edit
-                                </a>
-                                <form action="{{ route('admins.matakuliah.destroy', $mk) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus mata kuliah ini?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="fa fa-trash"></i> Hapus
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="6" class="text-center">Tidak ada data mata kuliah.</td>
-                        </tr>
-                    @endforelse
+                    <tr>
+                        <td colspan="6" class="text-center">Hello World</td>
+                    </tr>
                 </tbody>
             </table>
-        </div>
-         <div class="d-flex justify-content-center">
-            {{ $matakuliah->links() }}
         </div>
     </div>
 </div>
