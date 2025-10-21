d4908460f33175a62438dd01d812036b8f17f7dc
Nama Kamu <email@example.com>
2025-10-21 20:38:33 +0800

    matakuliah

--- app/Models/Matakuliah.php
+++ app/Models/Matakuliah.php
@@ -8,22 +8,17 @@ class MataKuliah extends Model
 {
     protected $table = 'mata_kuliah';
     protected $primaryKey = 'kode_mk';
-
-    public $incrementing = false; // karena primary key bukan auto increment
-    protected $keyType = 'string';
-    protected $fillable = ['kode_mk','nama_mk','sks','semester','id_dosen'];
-
-    public function dosen() {
-        return $this->belongsTo(User::class, 'id_dosen', 'id');
-    }
-}
-
-    public $incrementing = false;   // PK bukan auto-increment
+    public $incrementing = false;
     protected $keyType = 'string';
 
-    protected $fillable = ['kode_mk','nama_mk','sks','semester','id_dosen'];
+    protected $fillable = [
+        'kode_mk',
+        'nama_mk',
+        'sks',
+        'semester',
+        'id_dosen'
+    ];
 
-    // <-- Tambahkan ini agar route model binding pakai kode_mk
     public function getRouteKeyName()
     {
         return 'kode_mk';
@@ -33,5 +28,4 @@ class MataKuliah extends Model
     {
         return $this->belongsTo(User::class, 'id_dosen', 'id');
     }
-
-
+}
